9397e513e5d15ec30a69fbc8735759b5ad10e290
xnxx1337 <153394129+xnxx1337@users.noreply.github.com>
2025-12-26 08:45:55 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -69,7 +69,7 @@
 
 -- library init
 	local library = {
-		directory = "Atlanta",
+		directory = "Easy-Cheat",
 		folders = {
 			"/fonts",
 			"/configs",
@@ -1528,7 +1528,7 @@
 
 			-- main window
 				local main_window = library:panel({
-					name = properties and properties.name or "Atlanta | ", 
+					name = properties and properties.name or "Easy-Cheat | ", 
 					size = dim2(0, 604, 0, 628),
 					position = dim2(0, (camera.ViewportSize.X / 2) - 302 - 96, 0, (camera.ViewportSize.Y / 2) - 421 - 12),
 					image = "rbxassetid://98823308062942",
@@ -1633,11 +1633,11 @@
 					image = "rbxassetid://115194686863276",
 				})
 
-				local watermark = library:watermark({default = os.date('Atlanta |  - %b %d %Y - %H:%M:%S')})  
+				local watermark = library:watermark({default = os.date('Easy-Cheat |  - %b %d %Y - %H:%M:%S')})  
 
 				task.spawn(function()
 					while task.wait(1) do 
-						watermark.change_text(os.date('Atlanta - Beta - %b %d %Y - %H:%M:%S'))
+						watermark.change_text(os.date('Easy-Cheat - Beta - %b %d %Y - %H:%M:%S'))
 					end 
 				end) 
 
